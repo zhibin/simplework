9456fdb03d2725a61b83b614fe0db85dcfa58ed8
zhibin <wzbwzb_187@163.com>
2012-01-13 00:40:36 +0800

    format

--- Simple/Controller.php
+++ Simple/Controller.php
@@ -1,21 +1,18 @@
 <?php
 class Simple_Controller
 {
-	public $request;
-	public $response;
-	public $map;
-	public function __construct($map, $request, $response)
-	{
-		$this->map = $map;
-		$this->request = $request;
-		$this->response = $response;
-		$this->init();
-	}
-
-	protected function init()
-	{
-	}
-    public  function end()
+    public $request;
+    public $response;
+    public $map;
+    public function __construct($map, $request, $response)
     {
+        $this->map = $map;
+        $this->request = $request;
+        $this->response = $response;
+        $this->init();
     }
+    protected function init()
+    {}
+    public function end()
+    {}
 }
